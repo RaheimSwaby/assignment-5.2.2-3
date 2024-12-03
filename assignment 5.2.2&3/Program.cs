b0e24be63d81defa8a48721a18f531e32608326d
Rahei <Rahei@RAHEIMSPC>
2024-12-03 11:16:37 -0500

    Add project files.

---
+++ assignment 5.2.2&3/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace assignment_5._2._2_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("lets count to ten");
+            CountToTen(1);
+            Console.WriteLine();
+            Console.WriteLine("Counting down from 10");
+            Countdown(10);
+
+        }
+
+
+        static void CountToTen(int n)
+        {
+            if (n > 10)
+            {
+                return;
+            }
+            Console.Write(n + " ");
+
+
+            CountToTen(n + 1);
+        }
+
+
+        static void Countdown(int n)
+        {
+            if (n < 0)
+            {
+                return;
+            }
+            Console.Write(n + " ");
+            Countdown(n - 1);
+        }
+    }
+}
+
